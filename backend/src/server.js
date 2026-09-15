a5192e841da91bc1278f075abab9366e49adfe8b
Bhavesh Banait <bhaveshbanait57@gmail.com>
2026-09-15 22:29:16 +0530

    feat(api): register dataset routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const organizationRoutes = require("./routes/organizationRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+const datasetRoutes = require("./routes/datasetRoutes");
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/organizations", organizationRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/datasets", datasetRoutes);
 
 app.listen(PORT, () => {
     console.log(`DataPulse API running on port ${PORT}`);
